ef8c03a497072a46940539313d833b6dd5a284b1
DonRaijin <donbounleuang@gmail.com>
2025-09-09 11:57:42 -0700

    Add files via upload

---
+++ ArrayPractice.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ArrayPractice {
+    public static void main(String[] args) {
+
+        String[] cars = new String[4]; // size is 4
+
+        cars[0] = "RX7";
+        cars[1] = "R34";
+        cars[2] = "NSX";
+        cars[3] = "911GT";
+
+        for (int i=0; i<4; i++) {
+        }
+        System.out.println(cars[1]);
+
+
+    }
+}
