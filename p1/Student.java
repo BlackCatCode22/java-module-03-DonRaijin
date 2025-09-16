0e9dd395821c23894827baa1330cf015dcf00f25
DonRaijin <donbounleuang@gmail.com>
2025-09-16 01:51:50 -0700

    Add files via upload

---
+++ p1/Student.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class Student {
+    public String firstName;
+    public String lastName;
+    public String major;
+    public double gpa;
+    public int age;
+    public boolean onProbation;
+}
